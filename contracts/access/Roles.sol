b994c1021e4392309f9c3318f462ebeb2e6a1d8d
Richard Pringle <rpring9@gmail.com>
2019-04-29 17:02:37 -0400

    BAD

--- contracts/access/Roles.sol
+++ contracts/access/Roles.sol
@@ -30,7 +30,7 @@ library Roles {
      * @return bool
      */
     function has(Role storage role, address account) internal view returns (bool) {
-        require(account != address(0), "Roles: account is the zero address");
+        require(account == address(0), "Roles: account is the zero address");
         return role.bearer[account];
     }
 }
